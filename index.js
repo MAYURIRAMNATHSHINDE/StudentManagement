4e622e9f301b97294a182439e7267d64c6dcc7c1
MAYURIRAMNATHSHINDE <mayurishinde24304@gmail.com>
2025-02-20 23:53:46 +0530

    student

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app=express()
 
 app.use(express.json())
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static('frontend'));
 
 app.use("/course",courseRoute)
 app.use("/student",studentRoute)
