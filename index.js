a01e27dec5bef8eb3f59e15df3f92643abadd5a1
MAYURIRAMNATHSHINDE <mayurishinde24304@gmail.com>
2025-02-20 23:42:31 +0530

    student

--- index.js
+++ index.js
@@ -10,12 +10,17 @@ const app=express()
 
 app.use(express.json())
 app.use(cors());
+app.use(express.static('public'));
+
 app.use("/course",courseRoute)
 app.use("/student",studentRoute)
-PORT=process.env.PORT
+PORT=process.env.PORT || 8080
 
-
-app.listen(PORT,()=>{
+app.get("/", (req, res) => {
+    res.send("Server is up and running!");
+  });
+  
+app.listen(PORT,'0.0.0.0',()=>{
     ConnectToDB()
     console.log("server started...")
 })
