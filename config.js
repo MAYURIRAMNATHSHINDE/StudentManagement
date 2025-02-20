a2713efa941fc957579b65d9c5b242714abbf905
MAYURIRAMNATHSHINDE <mayurishinde24304@gmail.com>
2025-02-20 21:45:27 +0530

    student

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 
 const ConnectToDB=async(req,res)=>{
-   await mongoose.connect(process.env.MONGO_URI)
+   await mongoose.connect(process.env.MONGO_URI,{
+      tlsAllowInvalidCertificates: true})
    console.log("Connected to DB")
 }
 
