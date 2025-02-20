548e9e3167bf13aed80370e84e659c834ada479d
MAYURIRAMNATHSHINDE <mayurishinde24304@gmail.com>
2025-02-20 22:01:10 +0530

    student

--- config.js
+++ config.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 
 const ConnectToDB=async(req,res)=>{
-   await mongoose.connect(process.env.MONGO_URI)
+   await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      tls: true
+    })
    console.log("Connected to DB")
 }
 
